ffc937ac08ce78a40eb8a0494575ae7dbd591819
Fahad-alqahtani <s.w.f1437@gmail.com>
2019-06-24 03:33:33 +0300

    Create BreakTime.py

---
+++ BreakTime.py
@@ -0,0 +1,12 @@
+import time
+import webbrowser
+
+takeAwake = 0
+print("take a break" + time.ctime())
+while takeAwake < 3:
+    # ! take break every 3 times for   30 minutes
+    time.sleep(2)
+    # the link for the song
+    webbrowser.open("https://www.youtube.com/watch?v=Eb_TnPuC64k")
+    # incremting
+    takeAwake += 1
