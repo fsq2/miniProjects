627914d9e1578f9dc56930b1aee4faa89c9e432f
Fahad-alqahtani <s.w.f1437@gmail.com>
2019-06-28 14:21:51 +0300

    Update phone.py

--- phone.py
+++ phone.py
@@ -1,15 +1,15 @@
 from twilio.rest import Client
 
 # Your Account SID from twilio.com/console
-account_sid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+account_sid = "ACecc34eea21b508523924bedb3a0c5423"
 # Your Auth Token from twilio.com/console
-auth_token = "your_auth_token"
+auth_token = "e3190ce49fcf9eee9cf265895f96ee4b"
 
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-    to="+15558675309",
-    from_="+15017250604",
-    body="Hello from Python!")
+    to="+1 ",  # ! the number you want to send the message
+    from_="+13035365929",  # ! twilio given number
+    body="Hello from Python!")  # ! the message body
 
 print(message.sid)
